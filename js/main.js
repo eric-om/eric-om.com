3d3d3812aafc83a8aaa341674404c9de6d316b19
Eric Om <om@berkeley.edu>
2021-09-08 20:55:36 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  $("body").addClass("bg");
+  $(window).scroll(function () {
+    var windowPos = $(this).scrollTop();
+    $(".fade").each(function (index) {
+      var blockHeight = $(this).outerHeight();
+      var blockPos = $(this).offset().top;
+      var opacity = 1 - Math.abs((3 * (windowPos - blockPos)) / blockHeight);
+      $(this).css("opacity", opacity);
+    });
+
+    $(".white").each(function (index) {
+      var blockHeight = $(this).outerHeight();
+      var blockPos = $(this).offset().top;
+      var opacity = 1 - Math.abs((3 * (windowPos - blockPos)) / blockHeight);
+      if (opacity > 0.2) {
+        $("body").removeClass("bg");
+      } else {
+        $("body").addClass("bg");
+      }
+    });
+  });
+});
